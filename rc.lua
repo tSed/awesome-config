f00343194c303ff5d7c48cf3b4010efa2696d56d
Samuel MARTIN <s.martin49@gmail.com>
2011-12-24 16:35:01 +0100

    rc.lua: change default layout

--- rc.lua
+++ rc.lua
@@ -71,7 +71,10 @@ layouts =
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+-- >>> Sam's config:
+--    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[2])
+-- <<< Sam's config.
 end
 -- }}}
 
