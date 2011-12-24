7bfdf284bf4a7a8b6f95527726ceeabac6b8abc2
Samuel MARTIN <s.martin49@gmail.com>
2011-12-24 17:00:33 +0100

    rc.lua: setup my own keybinding (to move across screens, tags, clients)

--- rc.lua
+++ rc.lua
@@ -205,16 +205,47 @@ root.buttons(awful.util.table.join(
 
 -- {{{ Key bindings
 globalkeys = awful.util.table.join(
-    awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
-    awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
+-- >>> Sam's config:
+--    awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
+--    awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
+--    awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
+--
+--    awful.key({ modkey,           }, "j",
+--        function ()
+--            awful.client.focus.byidx( 1)
+--            if client.focus then client.focus:raise() end
+--        end),
+--    awful.key({ modkey,           }, "k",
+--        function ()
+--            awful.client.focus.byidx(-1)
+--            if client.focus then client.focus:raise() end
+--        end),
+--    awful.key({ modkey,           }, "w", function () mymainmenu:show({keygrabber=true}) end),
+--
+--    -- Layout manipulation
+--    awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end),
+--    awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end),
+--    awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end),
+--    awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end),
+--    awful.key({ modkey,           }, "u", awful.client.urgent.jumpto),
+--    awful.key({ modkey,           }, "Tab",
+--        function ()
+--            awful.client.focus.history.previous()
+--            if client.focus then
+--                client.focus:raise()
+--            end
+--        end),
+
+    awful.key({ modkey, "Control" }, "Left",   awful.tag.viewprev       ),
+    awful.key({ modkey, "Control" }, "Right",  awful.tag.viewnext       ),
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
 
-    awful.key({ modkey,           }, "j",
+    awful.key({ modkey,           }, "Left",
         function ()
             awful.client.focus.byidx( 1)
             if client.focus then client.focus:raise() end
         end),
-    awful.key({ modkey,           }, "k",
+    awful.key({ modkey,           }, "Right",
         function ()
             awful.client.focus.byidx(-1)
             if client.focus then client.focus:raise() end
@@ -222,10 +253,10 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "w", function () mymainmenu:show({keygrabber=true}) end),
 
     -- Layout manipulation
-    awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end),
-    awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end),
-    awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end),
-    awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end),
+    awful.key({ modkey, "Shift"   }, "Up", function () awful.client.swap.byidx(  1)    end),
+    awful.key({ modkey, "Shift"   }, "Down", function () awful.client.swap.byidx( -1)    end),
+    awful.key({ modkey, "Shift"   }, "Left", function () awful.screen.focus_relative( 1) end),
+    awful.key({ modkey, "Shift"   }, "Right", function () awful.screen.focus_relative(-1) end),
     awful.key({ modkey,           }, "u", awful.client.urgent.jumpto),
     awful.key({ modkey,           }, "Tab",
         function ()
@@ -234,6 +265,8 @@ globalkeys = awful.util.table.join(
                 client.focus:raise()
             end
         end),
+-- <<< Sam's config.
+
 
     -- Standard program
 -- >>> Sam's config:
