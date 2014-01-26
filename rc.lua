50e8038c4dda121ff8b0b421b3340e9d65b27005
Samuel Martin <s.martin49@gmail.com>
2014-01-26 12:41:12 +0100

    rc.lua: update terminal

--- rc.lua
+++ rc.lua
@@ -47,7 +47,7 @@ end
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "gnome-terminal"
+terminal = "terminology"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 browsers = { "chromium", "epiphany", "firefox" }
